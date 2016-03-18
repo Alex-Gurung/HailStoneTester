954021d9275218e65ff81cae962c4ff74b7bfa09
Alex Gurung <Alex Gurung>
2016-03-17 20:35:22 -0400

    Changed some numbers to unsigned long long ints

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "helper.h"
 
 void testnums(unsigned long long int hail){
-    long long orghail = hail;
+    unsigned long long int orghail = hail;
     while(hail!= 0){
         hail = testHail(hail);
     }
@@ -12,7 +12,7 @@ void testnums(unsigned long long int hail){
 }
 int main(){
     unsigned long long int counter = 10000;
-    for(long long i = 5476377146882523136; i<18446744073709551615 ; i++){ 
+    for(unsigned long long int i = 5476377146882523136; i<18446744073709551615 ; i++){ 
 //18446744073709551615 is the largest value available for an unsigned long long int, and 5476377146882523136 was the previous largest tested value
         testnums(i);
     }

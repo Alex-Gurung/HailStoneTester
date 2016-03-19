172e1458f9ce33442ee8bd936e7a2f5ec4828af9
Alex Gurung <aag1234@gmail.com>
2016-03-19 16:53:18 -0400

    Added two comments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 //
 // Created by Alex Gurung
 //
-#include "helper.h"
+#include "helper.h" //Contains the helper method to find the next hailstone number
 
-void testnums(unsigned long long int hail){
+void testnums(unsigned long long int hail){ //Tests the given number by going through each iteration of the hailstone sequence until it gets to zero
     unsigned long long int orghail = hail;
     while(hail!= 0){
         hail = testHail(hail); //Within helper.h file, testHail returns the next hailstone number, or 0 if you get to 4
@@ -11,7 +11,7 @@ void testnums(unsigned long long int hail){
     cout << orghail << endl; //This is a test commit
 }
 int main(){
-    unsigned long long int counter = 10000;
+    //unsigned long long int counter = 10000;
     for(unsigned long long int i = 5476377146882523136; i<18446744073709551615 ; i++){ 
 //18446744073709551615 is the largest value available for an unsigned long long int, and 5476377146882523136 was the previous largest tested value
         testnums(i);

47ce2f8915813535a2c4c72339b5a0365405d6eb
Alex Gurung <aag1234@gmail.com>
2016-03-22 21:05:29 -0400

    Changed types in main Changed all unsigned long long ints to just unsigned long long

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 //
 #include "helper.h" //Contains the helper method to find the next hailstone number
 
-void testnums(unsigned long long int hail){ //Tests the given number by going through each iteration of the hailstone sequence until it gets to zero
+void testnums(unsigned long long hail){ //Tests the given number by going through each iteration of the hailstone sequence until it gets to zero
     unsigned long long int orghail = hail;
     while(hail!= 0){
         hail = testHail(hail); //Within helper.h file, testHail returns the next hailstone number, or 0 if you get to 4
@@ -12,7 +12,7 @@ void testnums(unsigned long long int hail){ //Tests the given number by going th
 }
 int main(){
     //unsigned long long int counter = 10000;
-    for(unsigned long long int i = 5476377146882523136; i<18446744073709551615 ; i++){ 
+    for(unsigned long long i = 5476377146882523136; i<18446744073709551615 ; i++){ 
 //18446744073709551615 is the largest value available for an unsigned long long int, and 5476377146882523136 was the previous largest tested value
         testnums(i);
     }
